e673e2f923dde6f3689ce8ccc00ef0b2790282fa
cxx <954669193@qq.com>
2018-01-22 22:39:43 +0800

    增加接单模块

---
+++ src/views/order.vue
@@ -0,0 +1,32 @@
+<!-- 用户信息 -->
+<template>
+     <div class="pl20 pr20">
+    <h2>以下可接单</h2>
+
+   
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "userInfo",
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+.info {
+  background: #fff;
+  h3{
+    margin-top: 30px;
+    font-size: 35px;
+    color:#000
+    
+  }
+
+}
+</style>
